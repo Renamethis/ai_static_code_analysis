668b742fdf09c81657e701cfce5de6cdc1ef44f3
Гаврилов Иван Сергеевич <isgavrilov@edu.hse.ru>
2025-02-01 18:27:04 +0300

    Move llama drafts to draft/ folder

--- draft/llama.py
+++ draft/llama.py
@@ -1,25 +1,27 @@
+from huggingface_hub import login
+import sys
 from datasets import load_dataset
 from transformers import AutoTokenizer
 
 # Load your CSV dataset
 # The dataset should have two columns: "vulnerable_code" and "fixed_code"
-dataset = load_dataset("json", data_files="MSR_data_cleaned.json")
-
+dataset = load_dataset("json", data_files="dataset_tuned.json")
 # Split the dataset into training and validation sets
 dataset = dataset["train"].train_test_split(test_size=0.2)
-
+print('test')
 # Initialize the tokenizer for Llama
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")  # Replace with your desired Llama model
-
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")#"TinyPixel/Llama-2-7B-bf16-sharded")  # Replace with your desired Llama model
+print('test2')
 # Tokenize the data
 def tokenize_function(example):
+    print(len(example))
     # Prepare input as "Fix the vulnerability: [vulnerable_code]" and target as "fixed_code"
     example["input_text"] = f"Fix the vulnerability: {example['func_before']}"
     example["target_text"] = example["vul_func_with_fix"]
     
     # Tokenize input and target
-    input_encodings = tokenizer(example["input_text"], truncation=True, padding="max_length", max_length=512)
-    target_encodings = tokenizer(example["target_text"], truncation=True, padding="max_length", max_length=512)
+    input_encodings = tokenizer(example["input_text"], truncation=True, max_length=512)
+    target_encodings = tokenizer(example["target_text"], truncation=True, max_length=512)
 
     # Return the encodings as input_ids and labels
     return {
@@ -27,17 +29,17 @@ def tokenize_function(example):
         "attention_mask": input_encodings["attention_mask"],
         "labels": target_encodings["input_ids"]
     }
-
 # Apply tokenization to the dataset
-tokenized_dataset = dataset.map(tokenize_function, batched=True)
-
+tokenized_dataset = dataset.map(tokenize_function, batched=True, batch_size=10, writer_batch_size=10)
+print('test3')
 # Set the dataset format for PyTorch
 tokenized_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
+
 import torch
 from transformers import AutoModelForCausalLM, Trainer, TrainingArguments
 
 # Load the pre-trained Llama model for fine-tuning
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B")
 
 # Define training arguments
 training_args = TrainingArguments(
